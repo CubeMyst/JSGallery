776a95b9419fe2a349611c29a0c3d5a154aa5af1
CubeMyst <yeiser192@gmail.com>
2023-10-24 15:37:13 -0400

    Add project 1

---
+++ 1-update-bgcolor/README.md
@@ -0,0 +1,11 @@
+# proyecto 1 - Change BgColor
+
+Actualizar el fondo de color de una web.
+
+## Requisitos:
+
+- [] Que cambie con un contador de tiempo.
+- [] Que recorra la gama de colores del rojo, luego del verde, luego del azul.
+- [] Que se detenga si pulsas un botón. Arranque de nuevo al volver a pulsar.
+
+[Ver ejemplo de Cambiar el color de fondo](https://codepen.io/jagadeshanh/details/YGJmmv)
---
+++ 1-update-bgcolor/main.js
@@ -0,0 +1,27 @@
+const body = document.body;
+const btnToggle = document.getElementById('btn-toggle');
+
+let currentHue = 0;
+let currentSaturation = 0;
+let intervalID;
+let isRunning = false;
+
+function changeBgColor() {
+  body.style.background = `hsl(${currentHue}, 100%, 50%)`;
+  currentHue = (currentHue + 1) % 360;
+  currentSaturation = (currentSaturation + 1) % 100
+};
+
+function toggleAnimation() {
+  if (isRunning) {
+    clearInterval(intervalID);
+    btnToggle.textContent = 'start';
+  } else {
+    intervalID = setInterval(changeBgColor, 100);
+    btnToggle.textContent = 'stop';
+  };
+  isRunning = !isRunning;
+};
+
+btnToggle.addEventListener('click', toggleAnimation);
+toggleAnimation();
